b2c4fcfcb6d25f659d1432d3fea419e865183691
Lycat91 <158769561+Lycat91@users.noreply.github.com>
2026-03-28 16:53:45 -0500

    N/A

--- easycontroller_refactor/motor_user_config.c
+++ easycontroller_refactor/motor_user_config.c
@@ -30,9 +30,9 @@ int UART_SEND_INTERVAL_US = 250000; // 4 Hz
 float cruise_error = 1.0f;
 float cruise_increment = 1.0f; //miliamps
 float CRUISE_INCREMENT_MAX = 500;
-float kp=2.0f;
+float kp=32.0f;
 float ki=0.0f;
 float kd=0.0f;
-int CRUISE_MAX_CURRENT_MA = 8000; // 10 A — max current smart cruise can request (mA)
+int CRUISE_MAX_CURRENT_MA = 10000; // 10 A — max current smart cruise can request (mA)
 float target_speed_adjustment_factor = 0.0f; // Additive offset to DIS target speed (mph)
 
